7d2ef7a58692297b3a427023abec32fdfd76214c
ShaneeexD <31047364+ShaneeexD@users.noreply.github.com>
2026-06-26 03:02:43 +0100

    Add custom ammo as child items inside trader-sold ammo boxes

--- Server/Services/TraderManager.cs
+++ Server/Services/TraderManager.cs
@@ -55,7 +55,7 @@ public static class TraderManager
                         StockCount = traderEntry.StockCount,
                         BuyRestrictionMax = traderEntry.BuyRestrictionMax,
                     };
-                    AddToTrader(def.AmmoBox.Id, def.AmmoBox.Name, boxTraderEntry, traders, logger);
+                    AddBoxToTrader(def, boxTraderEntry, traders, logger);
                 }
                 catch (Exception ex)
                 {
@@ -115,5 +115,72 @@ public static class TraderManager
         assort.LoyalLevelItems[assortItemId] = traderEntry.LoyaltyLevel;
 
         logger.LogWithColor($"[AmmoGen] Added {itemName} to trader '{traderEntry.TraderId}' (LL{traderEntry.LoyaltyLevel}, {traderEntry.PriceRoubles}₽)", LogTextColor.Green);
+    }
+
+    private static void AddBoxToTrader(AmmoDefinition def, TraderEntry traderEntry, Dictionary<MongoId, Trader> traders, ISptLogger<AmmoGenPlugin> logger)
+    {
+        var box = def.AmmoBox;
+        MongoId traderId = new MongoId(traderEntry.TraderId);
+
+        if (!traders.TryGetValue(traderId, out var trader))
+        {
+            logger.LogWithColor($"[AmmoGen] Trader '{traderEntry.TraderId}' not found for ammo box '{box.Name}'. Skipping.", LogTextColor.Red);
+            return;
+        }
+
+        var assort = trader.Assort;
+        if (assort == null)
+        {
+            logger.LogWithColor($"[AmmoGen] Trader '{traderEntry.TraderId}' has no assort. Skipping ammo box '{box.Name}'.", LogTextColor.Red);
+            return;
+        }
+
+        MongoId boxAssortId = new MongoId(box.Id);
+
+        var boxItem = new Item
+        {
+            Id = boxAssortId,
+            Template = new MongoId(box.Id),
+            ParentId = "hideout",
+            SlotId = "hideout",
+            Upd = new Upd
+            {
+                StackObjectsCount = traderEntry.StockCount,
+                UnlimitedCount = false,
+                BuyRestrictionMax = traderEntry.BuyRestrictionMax,
+                BuyRestrictionCurrent = 0,
+            }
+        };
+        assort.Items.Add(boxItem);
+
+        // Add the custom ammo as a child item inside the box
+        var ammoChild = new Item
+        {
+            Id = new MongoId().ToString(),
+            Template = new MongoId(def.Id),
+            ParentId = boxAssortId,
+            SlotId = "cartridges",
+            Upd = new Upd
+            {
+                StackObjectsCount = box.Count,
+            }
+        };
+        assort.Items.Add(ammoChild);
+
+        var barterEntry = new List<List<BarterScheme>>
+        {
+            new List<BarterScheme>
+            {
+                new BarterScheme
+                {
+                    Count = traderEntry.PriceRoubles,
+                    Template = new MongoId(RoublesTpl),
+                }
+            }
+        };
+        assort.BarterScheme[boxAssortId] = barterEntry;
+        assort.LoyalLevelItems[boxAssortId] = traderEntry.LoyaltyLevel;
+
+        logger.LogWithColor($"[AmmoGen] Added ammo box {box.Name} to trader '{traderEntry.TraderId}' (LL{traderEntry.LoyaltyLevel}, {traderEntry.PriceRoubles}₽) with {box.Count} rounds", LogTextColor.Green);
     }
 }
