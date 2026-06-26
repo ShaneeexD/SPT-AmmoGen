ce3d5b1bdfbf6e10c0afd1846ed345c5651bfdd3
ShaneeexD <31047364+ShaneeexD@users.noreply.github.com>
2026-06-26 16:19:46 +0100

    Remove PowerShell script for inspecting Item class from SPT assembly

--- inspect_item.ps1
+++
@@ -1,5 +0,0 @@
-$asm = [System.Reflection.Assembly]::LoadFrom('C:\SPT\SPT\SPTarkov.Server.Core.dll')
-$type = $asm.GetTypes() | Where-Object { $_.Name -eq 'Item' -and $_.FullName -like '*Eft*' } | Select-Object -First 1
-if ($type -eq $null) { $type = $asm.GetTypes() | Where-Object { $_.Name -eq 'Item' } | Select-Object -First 1 }
-$type | Format-List FullName, BaseType
-$type.GetProperties() | Select-Object Name, PropertyType | Format-Table -AutoSize
